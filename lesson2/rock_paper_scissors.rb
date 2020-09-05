f00fd17501ca412d9716e7f9349db1994c950af9
Julia Shi <julia.shi@nyu.edu>
2020-09-05 12:19:16 +0800

    update rps

--- lesson2/rock_paper_scissors.rb
+++ lesson2/rock_paper_scissors.rb
@@ -1,75 +1,137 @@
-=begin
-1. Lizard Spock addition
-need to add additional logic to display results
-2. Abbreviation for words
-need to re-validate choices
-3. keep scores
-need to add additional loops
-=end
+require 'yaml'
 
 VALID_CHOICES = { 'r' => 'rock', 'p' => 'paper', 'sc' => 'scissors',
                   'l' => 'lizard', 'sp' => 'spock' }
 RULES = { 'r' => ['sc', 'l'], 'p' => ['r', 'sp'], 'sc' => ['p', 'l'],
           'l' => ['sp', 'p'], 'sp' => ['r', 'sc'] }
+MESSAGES = YAML.load(
+           File.open('rps_messages.yml').read
+           )
+NUM_WINS = 5
 
 def prompt(message)
   puts "=> #{message}"
+end
+
+def pause(time = 1.0)
+  sleep(time)
 end
 
 def win?(first, second)
   RULES[first].include?(second)
 end
 
-def results(player, computer)
-  if win?(player, computer)
-    "You won!"
-  elsif win?(computer, player)
-    "Computer won!"
-  else
-    "It's a tie!"
+def begin_game?
+  loop do
+    begin_choice = valid_yes_no(MESSAGES["begin"])
+    if begin_choice.start_with?('y')
+      break prompt(MESSAGES['game_start'])
+    else
+      quit_game
+    end
   end
 end
 
-prompt("Welcome to the #{VALID_CHOICES.values.join(' ')} game!")
+def quit_game
+  abort MESSAGES['game_end']
+end
 
-computer_wins = 0
-player_wins = 0
-while computer_wins < 5 && player_wins < 5
+def get_choice
   choice = ''
   loop do
-    prompt("Please choose one: #{VALID_CHOICES.keys.join(',')}
-           (for rock, paper, scissors, spock or lizard)")
+    prompt(MESSAGES['choice'])
     choice = gets.chomp.downcase
     if VALID_CHOICES.key?(choice)
       break
     else
-      prompt("That's not a valid choice.")
+      prompt(MESSAGES['not_valid'])
     end
   end
+  choice
+end
+
+def results(player, computer)
+  if win?(player, computer)
+    MESSAGES["player_won"]
+  elsif win?(computer, player)
+    MESSAGES["computer_won"]
+  else
+    MESSAGES["tie"]
+  end
+end
+
+def update_score(winner_message, score_hash)
+  if winner_message.include?("You")
+    score_hash["player_wins"] += 1
+  elsif winner_message.include?("Computer")
+    score_hash["computer_wins"] += 1
+  end
+end
+
+def valid_yes_no(question)
+  input = ''
+  prompt(question)
+  loop do
+    input = gets.chomp.downcase
+    break if %w(y yes n no).include?(input)
+    prompt(MESSAGES["not_valid"])
+  end
+  input
+end
+
+def display_score(hash)
+  prompt("Computer scored #{hash['computer_wins']}; You scored #{hash['player_wins']}")
+end
+
+def display_grand_winner(player, computer)
+  if player == 5
+    prompt("The match is over! You are the grand winner!")
+  elsif computer == 5
+    prompt("Computer won. Try again next time")
+  else
+    prompt("You won #{player} times and computer won #{computer} times. There is no grand winner.")
+  end
+end
+
+system('cls')
+
+prompt(MESSAGES["welcome"])
+pause
+prompt(MESSAGES["rules"])
+pause
+prompt(MESSAGES["win_condition"] + " " + "#{NUM_WINS}")
+pause
+
+begin_game?
+pause
+system('cls')
+
+scores = {'computer_wins' => 0,
+          'player_wins' => 0 }
+
+while scores['computer_wins'] < NUM_WINS && scores['player_wins'] < NUM_WINS
+
+  player_choice = get_choice
 
   computer_choice = VALID_CHOICES.keys.sample
-  puts "you chose: #{choice}; Computer chose: #{computer_choice}"
 
-  player_result = results(choice, computer_choice)
+  puts "You chose: #{VALID_CHOICES[player_choice]}; Computer chose: #{VALID_CHOICES[computer_choice]}"
+  pause
+
+  player_result = results(player_choice, computer_choice)
   prompt(player_result)
+  pause(2)
 
-  if player_result.include?("You")
-    player_wins += 1
-  elsif player_result.include?("Computer")
-    computer_wins += 1
-  end
+  update_score(player_result, scores)
+  display_score(scores)
+  pause
 
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  player_continue = valid_yes_no(MESSAGES["continue"])
+  break if player_continue.start_with?('n')
+
 end
 
-if player_wins == 5
-  prompt("The match is over! You are the grand winner!")
-elsif computer_wins == 5
-  prompt("Computer won. Try again next time")
-else
-  prompt("You won #{player_wins} times and computer won #{computer_wins} times")
-end
+display_grand_winner(scores['player_wins'], scores['computer_wins'])
+pause
 
 prompt("Thank you for playing.")
