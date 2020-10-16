6925a922b07f49d19aed4de8972ed1b77ea83c8d
Julia Shi <julia.shi@nyu.edu>
2020-10-16 10:22:56 +0800

    easy1

---
+++ small_problems/easy1/odd.rb
@@ -0,0 +1,13 @@
+#rules, takes on integer argument (valid), returns true if the number's absolute value is odd
+# input: integer
+# output: return boolean
+
+def is_odd?(number)
+  number % 2 == 1
+end
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
